e37e84e223d9f96cb73c28535396b607964941e6
ralmond-nhs-e-i <77148793+ralmond-nhs-e-i@users.noreply.github.com>
2021-02-10 16:42:03 +0000

    add plotting script, first save complex study defn

---
+++ analysis/basic_plot_script.R
@@ -0,0 +1,54 @@
+library('here')
+
+library('tidyverse')
+
+## import data
+df_input <- read_csv(
+  here::here("output", "input_simple.csv"), 
+  col_types = cols(
+    patient_id = col_integer(),
+    stp = col_character()
+  )
+)
+
+# count STP for each registered patient
+df_stppop = df_input %>% count(stp, name='registered')
+
+plot_stppop_bar <- df_stppop %>%
+  mutate(
+    name = forcats::fct_reorder(stp, registered, median, .desc=FALSE)
+  ) %>%
+  ggplot() +
+  geom_col(aes(x=registered/1000000, y=name, fill=registered), colour='black') +
+  scale_fill_gradient(limits = c(0,NA), low="white", high="blue", guide=FALSE)+
+  labs(
+    title="TPP-registered patients per STP",
+    subtitle= "as at 1 January 2020",
+    y=NULL,
+    x="Registered patients\n(million)",
+    fill = NULL)+
+  theme_minimal()+
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    plot.title.position = "plot",
+    plot.caption.position =  "plot"
+  )
+plot_stppop_bar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ analysis/study_defintion_complex.py
@@ -0,0 +1,36 @@
+
+## draft space for creating a more complex study definition for learning disabilities research
+# semi based on ethnicity research repo
+
+## LIBRARIES 
+
+# cohort extractor
+from cohortextractor import (StudyDefinition, patients)
+
+
+# dictionaries of STP codes (for dummy data)
+from dictionaries import dict_stp
+
+# set the index date 
+index_date = "2020-01-01"
+
+## STUDY POPULATION
+
+study = StudyDefinition(
+  
+  default_expectations = {
+    "date": {"earliest": index_date, "latest": "today"}, # date range for simulation
+    "rate": "uniform",
+    "incidence": 1
+  }, 
+  
+  population = patients.registered_as_of(index_date),
+
+ stp = patients.registered_practice_as_of(
+        index_date,
+        returning="stp_code",
+        return_expectations={
+            "category": {"ratios": dict_stp},
+        }
+ )
+)
