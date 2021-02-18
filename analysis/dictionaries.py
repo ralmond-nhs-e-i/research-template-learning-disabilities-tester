f42d888941f6a50e3241c4077cb6e2cd38c616cf
ralmond-nhs-e-i <77148793+ralmond-nhs-e-i@users.noreply.github.com>
2021-02-18 14:03:53 +0000

    stp files

--- analysis/dictionaries.py
+++ analysis/dictionaries.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 STPs = pd.read_csv(
-    filepath_or_buffer = './codelists/STP.csv'
+    filepath_or_buffer = './lib/STP.csv'
 )
 dict_stp = { stp : 1/len(STPs.index) for stp in STPs['stp_id'].tolist() }
